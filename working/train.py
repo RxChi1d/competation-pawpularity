824bdb9dcc12366d462f2797276e9f9a275da657
kevin30205 <kevin30205@gmail.com>
2023-05-21 15:06:01 +0800

    swin_large_patch4_window7_224_in22k(f10_e50_bs32)

--- working/train.py
+++ working/train.py
@@ -44,8 +44,8 @@ from pytorch_lightning.loggers import TensorBoardLogger
 # In[ ]:
 
 
-config = {'exp_name':'exp_20230521-1',
-          'commit': "swin_large_patch4_window7_224(f10_e50_bs16)",
+config = {'exp_name':'exp_20230521-2',
+          'commit': "swin_large_patch4_window7_224_in22k(f10_e50_bs32)",
           'root': '../input/petfinder-pawpularity-score/',  # Data root
           'seed': 2023,
           'n_splits': 10,
@@ -58,20 +58,20 @@ config = {'exp_name':'exp_20230521-1',
                       'num_training': 100},
           'model':{
               'package': 'timm',  # timm or torchvision
-              'name': 'swin_large_patch4_window7_224',
+              'name': 'swin_large_patch4_window7_224_in22k',
               'output_dim': 1,
               'pretrain': True,
           },
-          'save_dir': 'swin_large_patch4_window7_224(f10_e50_bs16)',  # 儲存權重與log的資料夾
+          'save_dir': 'swin_large_patch4_window7_224_in22k(f10_e50_bs32)',  # 儲存權重與log的資料夾
           'train_loader': {
-              'batch_size': 16,
+              'batch_size': 32,
               'shuffle': True,
               'num_workers': os.cpu_count(),
               'pin_memory': True,
               'drop_last': False
           },
           'val_loader': {
-              'batch_size': 16,
+              'batch_size': 32,
               'shuffle': False,
               'num_workers': os.cpu_count(),
               'pin_memory': True,
